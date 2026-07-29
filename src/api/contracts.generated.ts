7d23647fa859d4e22531ae3b7ec1f9a953666ac0
SadPossum <aprokudanov@gmail.com>
2026-07-29 17:24:46 +0300

    Update Staff restriction API contracts

--- src/api/contracts.generated.ts
+++ src/api/contracts.generated.ts
@@ -3429,6 +3429,47 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/data-rights/tenant/cases/{caseId}/restriction": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path: {
+                    caseId: string;
+                };
+                cookie?: never;
+            };
+            requestBody: {
+                content: {
+                    "application/json": components["schemas"]["ExecuteDataRightsRestrictionRequest"];
+                };
+            };
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": components["schemas"]["DataRightsRestrictionExecutionDto"];
+                    };
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/api/data-rights/tenant/cases/{caseId}/correction": {
         parameters: {
             query?: never;
